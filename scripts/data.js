bd771a4d12e2ba0319f7f9e5fe2893897f3e01f0
ONE1VAN <ivan01082005@gmail.com>
2026-04-05 13:55:51 +0300

    improved code due to dry principe

---
+++ scripts/data.js
@@ -0,0 +1,65 @@
+const sections = [
+  {
+    id: 'flags',
+    title: 'Flags',
+    folder: 'flags',
+    href: 'flags.html',
+    count: 7
+  },
+  {
+    id: 'forest-game',
+    title: 'Forest Game',
+    folder: 'forest game',
+    href: 'forest-game.html',
+    count: 8
+  },
+  {
+    id: 'foundators',
+    title: 'Foundators',
+    folder: 'foundators',
+    href: 'foundators.html',
+    count: 9
+  },
+  {
+    id: 'ghillie-suit',
+    title: 'Ghillie Suit',
+    folder: 'ghillie suit',
+    href: 'ghillie-suit.html',
+    count: 11
+  },
+  {
+    id: 'miltec',
+    title: 'Miltec',
+    folder: 'miltec',
+    href: 'miltec.html',
+    count: 8
+  },
+  {
+    id: 'night-game',
+    title: 'Night Game',
+    folder: 'night game',
+    href: 'night-game.html',
+    count: 6
+  },
+  {
+    id: 'sniper-rifle',
+    title: 'Sniper Rifle',
+    folder: 'sniper rifle',
+    href: 'sniper-rifle.html',
+    count: 5
+  },
+  {
+    id: 'sqb',
+    title: 'SQB',
+    folder: 'SQB',
+    href: 'sqb.html',
+    count: 9
+  },
+  {
+    id: 'the-best-programmer',
+    title: 'The Best Programmer',
+    folder: 'the best programmer',
+    href: 'the-best-programmer.html',
+    count: 6
+  }
+];
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,22 +1,10 @@
-const sectionFolders = [
-  { folder: 'flags', title: 'Flags', href: 'flags.html' },
-  { folder: 'forest game', title: 'Forest Game', href: 'forest-game.html' },
-  { folder: 'foundators', title: 'Foundators', href: 'foundators.html' },
-  { folder: 'ghillie suit', title: 'Ghillie Suit', href: 'ghillie-suit.html' },
-  { folder: 'miltec', title: 'Miltec', href: 'miltec.html' },
-  { folder: 'night game', title: 'Night Game', href: 'night-game.html' },
-  { folder: 'sniper rifle', title: 'Sniper Rifle', href: 'sniper-rifle.html' },
-  { folder: 'SQB', title: 'SQB', href: 'sqb.html' },
-  { folder: 'the best programmer', title: 'The Best Programmer', href: 'the-best-programmer.html' }
-];
-
 function initIndexPage() {
   const sectionLinks = document.getElementById('sectionLinks');
   const heroPhotos = document.getElementById('heroPhotos');
 
   if (sectionLinks) {
     sectionLinks.innerHTML = '';
-    sectionFolders.forEach((cat) => {
+    sections.forEach((cat) => {
       const link = document.createElement('a');
       link.href = cat.href;
       link.textContent = cat.title;
@@ -25,7 +13,7 @@ function initIndexPage() {
   }
 
   if (heroPhotos) {
-    sectionFolders.forEach((cat) => {
+    sections.forEach((cat) => {
       const coverUrl = `images/${encodeURIComponent(cat.folder)}/pic%201.jpg`;
       const card = document.createElement('a');
       card.href = cat.href;
